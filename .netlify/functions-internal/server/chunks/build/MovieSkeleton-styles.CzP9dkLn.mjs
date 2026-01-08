d11d6e79897bb805a531f2c7a6901384a1bcc6cf
maktub_zyc <13873326870@163.com>
2026-01-08 12:02:26 +0800

    项目config精简完整版

--- .netlify/functions-internal/server/chunks/build/MovieSkeleton-styles.CzP9dkLn.mjs
+++
@@ -1,8 +0,0 @@
-const MovieSkeleton_vue_vue_type_style_index_0_scoped_fd840bcf_lang = "@keyframes pulse-fd840bcf{0%,to{opacity:1}50%{opacity:.5}}";
-
-const MovieSkeletonStyles_CzP9dkLn = [
-  MovieSkeleton_vue_vue_type_style_index_0_scoped_fd840bcf_lang
-];
-
-export { MovieSkeletonStyles_CzP9dkLn as default };
-//# sourceMappingURL=MovieSkeleton-styles.CzP9dkLn.mjs.map
--- .netlify/functions-internal/server/chunks/build/error-styles.D4iVAQHJ.mjs
+++
@@ -1,8 +0,0 @@
-const error_vue_vue_type_style_index_0_scoped_2a5d79fa_lang = "@keyframes bounce-2a5d79fa{0%,to{transform:translateY(0)}50%{transform:translateY(-20px)}}.animate-bounce[data-v-2a5d79fa]{animation:bounce-2a5d79fa 2s infinite}";
-
-const errorStyles_D4iVAQHJ = [
-  error_vue_vue_type_style_index_0_scoped_2a5d79fa_lang
-];
-
-export { errorStyles_D4iVAQHJ as default };
-//# sourceMappingURL=error-styles.D4iVAQHJ.mjs.map
